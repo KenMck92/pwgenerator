12ebbaf6c94f138d3ee14d31c64a42ea038afa7b
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-06-29 14:48:23 -0500

    added pseudocode to js

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // prompt how many characters
+  // (stores)var for characters
+
+  // What types of characters
+  // special, number, upper, lower
+  // window.confirm
+
+  // Use answers to make password
+  // vars that include all possible characters
+  // seperate vars for each type, arrays
+  // example: var upperLetters = ["A", "B", "C"]
+
+  // take all true arrays and concat into new array
+  // choose random characters from arr based on user input
+  // var push into new array 
+  // var password = turn array into string .toString()
+
+  // return password
+}
