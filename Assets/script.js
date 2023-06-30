48df2acc124454eb015f9b49fe453bd75da3d104
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-06-29 23:47:41 -0500

    updated js

--- Assets/script.js
+++ Assets/script.js
@@ -16,9 +16,24 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   // prompt how many characters
   // (stores)var for characters
+  var characterNumber = window.prompt("How many characters");
+  console.log(characterNumber);
+  
+  if (characterNumber < 8 || characterNumber > 128) {
+    window.alert("Quanity has to be at least 8 and no more than 12");
+    return;
+  }
 
   // What types of characters
   // special, number, upper, lower
+  var specialCharacter = window.confirm("Do you want special characters?");
+  console.log(specialCharacter);
+  var numbers = window.confirm("Do you want numbers?");
+  console.log(numbers);
+  var upperCase = window.confirm("Do you want uppercase letters?");
+  console.log(upperCase);
+  var lowerCase = window.confirm("Do you want lowercase letters?");
+  console.log(lowerCase);
   // window.confirm
 
   // Use answers to make password
